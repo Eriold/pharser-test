1816029cf06ee72337ad6b4f7e46f0b1d0a53ee7
Daniel Montoya <dacamoty@gmail.com>
2026-06-01 23:03:05 -0500

    correction of route vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 
-const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1];
 const isGitHubPages = process.env.GITHUB_PAGES === "true";
 
 export default defineConfig({
-  base: isGitHubPages && repoName ? `/${repoName}/` : "/"
+  base: isGitHubPages ? "./" : "/"
 });
